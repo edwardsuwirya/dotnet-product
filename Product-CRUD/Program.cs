3692a17e2e47c3512547ab2faeedaf6b47fb4da1
edwardsuwirya <edward_suwirya@yahoo.com>
2022-09-15 14:57:02 +0700

    Read and write to file

--- Product-CRUD/Program.cs
+++ Product-CRUD/Program.cs
@@ -8,10 +8,11 @@ public class Program
 
     public static void Main()
     {
-        IProductRepository productRepo = new ProductArrayRepository();
+        String filePath = "/Users/edwardsuwirya/Downloads/product.txt";
+        IProductRepository productRepo = new ProductFileRepository(filePath);
 
-        productRepo.Add(new Product("1", "Nasi Goreng"));
-        productRepo.Add(new Product("2", "Es teh tawar"));
+        productRepo.Add(new Product("3", "Mie Ayam"));
+        productRepo.Add(new Product("4", "Mie Baso"));
 
         var products = productRepo.GetAll();
         foreach (var p in products)
@@ -19,7 +20,7 @@ public class Program
             Console.WriteLine(p.ToString());
         }
 
-        var productResId = productRepo.FindById("2");
+        var productResId = productRepo.FindById("3");
         Console.WriteLine(productResId?.ToString() ?? "No Product Found");
 
     }
---
+++ Product-CRUD/Repository/ProductFileRepository.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using ProductCRUD.Model;
+
+namespace ProductCRUD.Repository
+{
+    public class ProductFileRepository : IProductRepository
+    {
+        private readonly string _filePath;
+
+        public ProductFileRepository(string filePath) => _filePath = filePath;
+
+        public List<Product> GetAll()
+        {
+            List<Product> products = new List<Product>();
+            using (StreamReader sr = new StreamReader(_filePath))
+            {
+                string line;
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] productStr = line.Split(",");
+                    if (productStr.Length == 2)
+                    {
+                        products.Add(new Product(productStr[0], productStr[1]));
+                    }
+                }
+            }
+            return products;
+        }
+
+        public void Add(Product product)
+        {
+            using (StreamWriter sw = new StreamWriter(_filePath, true))
+            {
+                sw.WriteLine($"{product.id},{product.productName}");
+            }
+        }
+
+        public Product? FindById(string id)
+        {
+            using (StreamReader sr = new StreamReader(_filePath))
+            {
+                string line;
+                Product product = null;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] productStr = line.Split(",");
+                    if (productStr[0] == id)
+                    {
+                        product = new Product(productStr[0], productStr[1]);
+                        break;
+                    }
+                }
+                return product;
+            }
+        }
+
+        public List<Product> FindByNameLike(string name)
+        {
+            List<Product> productRes = new List<Product>();
+            using (StreamReader sr = new StreamReader(_filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] productStr = line.Split(",");
+                    if (productStr[1].Contains(name))
+                    {
+                        var product = new Product(productStr[0], productStr[1]);
+                        productRes.Add(product);
+                    }
+                }
+            }
+            return productRes;
+        }
+    }
+}
+
